e3ad037b27d7124a562c912a74b0b51014a78fb2
Harshita_Mathpal <91454889+HarshitaMathpal@users.noreply.github.com>
2023-01-12 14:52:36 +0530

    Create Check if Tree is Isomorphic.java

---
+++ Check if Tree is Isomorphic.java
@@ -0,0 +1,22 @@
+class Solution  
+{ 
+    // Return True if the given trees are isomotphic. Else return False.
+    boolean isIsomorphic(Node root1, Node root2)  
+    { 
+         // code here.
+         if(root1 == null && root2 == null)
+            return true;
+        
+        if(root1 == null || root2 == null)
+            return false;
+            
+        if(root1.data != root2.data)
+            return false;
+            
+        boolean flip = isIsomorphic(root1.left,root2.right) && isIsomorphic(root1.right,root2.left);
+        boolean noflip = isIsomorphic(root1.right,root2.right) && isIsomorphic(root1.left,root2.left);
+        
+        return flip || noflip;
+    }
+    
+}    
